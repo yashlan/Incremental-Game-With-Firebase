b3043a197f50bd1bce5abdd42ebba910be28aa55
yashlan <yashlan007@gmail.com>
2021-09-30 21:25:20 +0700

    step 1 done :fire:

---
+++ Assets/Scripts/TapArea.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using Yashlan.manage;
+
+namespace Yashlan.ui
+{
+    public class TapArea : MonoBehaviour, IPointerDownHandler
+    {
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            GameManager.Instance.CollectByTap(eventData.position, transform);
+        }
+    }
+}
